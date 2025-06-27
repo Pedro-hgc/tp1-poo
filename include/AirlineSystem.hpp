069e980bb15bf46106089a9d9da6185f0afaed6a
Luiz Guilherme C. Da Silva <costadasilvaluizguilherme@gmail.com>
2025-06-27 19:26:01 -0300

    Prototypes from the AirlineSystem class started to be created

---
+++ include/AirlineSystem.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <vector>
+
+#include "Airplane.hpp"
+#include "Pilot.hpp"
+#include "Passenger.hpp"
+#include "Flight.hpp"
+
+class AirlineSystem {
+private:
+        std::vector<Airplane*> airplanes;
+        std::vector<Pilot*> pilots;
+        std::vector<Passenger*> passengers;
+        std::vector<Flight*> flights_avaibles;
+
+public:
+        AirlineSystem();
+        ~AirlineSystem();
+
+        // The Class Menu will have a system.
+        // Inside the class Menu will have options
+        // This options creates Airplanes, Pilots, Passengers
+        // After is created in the method from Menu, then
+        // The Object will be pushed to the correct vector
+        bool registerNewAirplane(Airplane* newAirplane);
+        bool registerNewPilot(Pilot* newPilot);
+        bool registerNewPassenger(Passenger* newPassenger);
+        bool registerNewFlight(Flight* newFlight);
+        bool boardPassenger(Passenger* passengerToBoard);
+
+        // This is used to get the INFO from All elements.
+        // Will be used by menu to show: the Passengers Registered
+        // Flights avaibles, Pilots registered, etc.
+        // Will show in the interface the information.
+        // So if a specif Pilot is wanted then will pass to
+        // getPilot(The license showed by showPilots())
+        void showAirplanes(void);
+        void showPilots(void);
+        void showPassengers(void);
+        void showFlightsAvaibles(void);
+
+        // This is used to get ONE element from the Vectors
+        Airplane*  getAirplane(std::string cod);
+        Pilot*     getPilot(std::string licenseNumber);
+        Passenger* getPassenger(std::string ticketNumber);
+        Flight*    getFlight(std::string code);
+
+};
